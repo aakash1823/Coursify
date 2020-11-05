463afc0f352b625035fa8de3cb0ae9e7eb850175
aakash1823 <48508563+aakash1823@users.noreply.github.com>
2020-11-05 18:00:03 +0530

    Add files via upload

---
+++ INTERNSHALA/1.js
@@ -0,0 +1,13 @@
+function data(){
+    document.getElementById('23').style.color= "black";
+    document.getElementById('25').style.color= "black";
+    document.getElementById('24').style.color= "black";
+
+
+  }
+  function data1(){
+    document.getElementById('23').style.color= "white";
+    document.getElementById('24').style.color= "white";
+    document.getElementById('25').style.color= "white";
+
+  }
